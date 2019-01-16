b97e04c7fd672a2851acc929ea2461708f723520
Nic Grounds <nic@nicandnat.com>
2019-01-15 18:45:02 -0600

    http server echos back query params

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var http = require('http');
 var server = http.createServer( function( req, res ) {
     res.writeHead( 200,
         { 'Content-Type' : 'text/plain' } );
-    res.write( `${req.url}` );
+    var url = require('url');
+    var parsedUrl = url.parse(req.url);
+    res.write( `${parsedUrl.query}` );
     res.end();
 } );
 server.listen( 8080 );
