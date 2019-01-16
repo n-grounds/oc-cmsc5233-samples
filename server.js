24a842bb26e585f90993247ba26eba288a1fd6bc
Nic Grounds <nic@nicandnat.com>
2019-01-15 18:54:30 -0600

    add error handling

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 var http = require('http');
 var server = http.createServer( function( req, res ) {
-    res.writeHead( 200,
-        { 'Content-Type' : 'text/plain' } );
+    
     var url = require('url');
     var parsedUrl = url.parse(req.url);
-    res.write(
-        JSON.stringify(
-            eval( parsedUrl.query ) ) );
+    try {
+        var responseText = eval( parsedUrl.query );
+        res.writeHead( 200,
+            { 'Content-Type' : 'text/plain' } );
+        res.write( JSON.stringify( responseText ) );
+    }
+    catch(err) {
+        res.writeHead( 500,
+            { 'Content-Type' : 'text/plain' } );
+        res.write( `Error evaluating ${parsedUrl.query}` );
+    }
     res.end();
 } );
 server.listen( 8080 );
