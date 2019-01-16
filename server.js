87869802263f7421715e19ae0fe67c297e2c1d78
Nic Grounds <nic@nicandnat.com>
2019-01-15 18:41:59 -0600

    http server print url

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var server = http.createServer( function( req, res ) {
     res.writeHead( 200,
         { 'Content-Type' : 'text/plain' } );
-    res.write( 'Hello, world' );
+    res.write( `${req.url}` );
     res.end();
 } );
 server.listen( 8080 );
