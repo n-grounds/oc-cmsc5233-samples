d9b0fa145c6a725ce6183a20a0f63168cde0d0b6
Nic Grounds <nic@nicandnat.com>
2019-01-15 18:33:30 -0600

    toString example

--- test.js
+++ test.js
@@ -2,4 +2,7 @@ var o = { firstname : 'Nicolas', lastname : 'Grounds',
         getName : function() {
             return this.firstname + ' ' + this.lastname;
         } };
-console.log( `Hello, ${o.getName()}` );
+o.toString = function() {
+    return this.getName();
+}
+console.log( `Hello, ${o}` );
