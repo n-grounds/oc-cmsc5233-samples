a2a5733bd0a0f15ddf003e1886b0557228c8262e
Nic Grounds <nic@nicandnat.com>
2019-01-29 19:22:25 -0600

    fun button to increment counter

--- app/home/home-page.js
+++ app/home/home-page.js
@@ -5,10 +5,23 @@ logic, and to set up your page’s data binding.
 */
 
 const HomeViewModel = require("./home-view-model");
+const view = require("tns-core-modules/ui/core/view");
 
 function onNavigatingTo(args) {
     const page = args.object;
     page.bindingContext = new HomeViewModel();
 }
 
+var counter = 0;
+
+function onTap(args) {
+    counter++;
+    const button = args.object;
+    const label = view.getViewById( button.parent, 'label' );
+    if( label ) {
+        label.text = `You've tapped ${counter} times`;
+    }
+}
+
 exports.onNavigatingTo = onNavigatingTo;
+exports.onTap = onTap;
